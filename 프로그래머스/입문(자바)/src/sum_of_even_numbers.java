0f0bde96c922a104c0a94f29fea5a84fbc039a96
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-01-01 19:31:15 +0900

    짝수들의 합

---
+++ 프로그래머스/입문(자바)/src/sum_of_even_numbers.java
@@ -0,0 +1,10 @@
+
+public class sum_of_even_numbers {
+	public int solution(int n) {
+		int answer = 0;
+        for(int i = 0; i<=n; i+=2) {
+                answer += i;
+        }
+        return answer;
+    }
+}
