13c73d93616c0f975a2c8728a9def619ac482515
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-01-05 23:05:28 +0900

    배열 원소의 길이

---
+++ 프로그래머스/입문(자바)/src/Length_of_array_element.java
@@ -0,0 +1,11 @@
+
+public class Length_of_array_element {
+	public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i = 0; i < strlist.length; i++) {
+            int length = strlist[i].length();
+            answer[i] = length;
+        }       
+        return answer;
+    }
+}
