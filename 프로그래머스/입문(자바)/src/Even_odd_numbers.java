bb0c4afad8a62cf270a36ca56c8ecfadc353c689
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-01-07 14:02:42 +0900

    짝수 홀수의 개수

---
+++ 프로그래머스/입문(자바)/src/Even_odd_numbers.java
@@ -0,0 +1,18 @@
+
+public class Even_odd_numbers {
+	public int[] solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+        int[] answer = new int[2];
+        for(int num : num_list) {
+            if(num%2==0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        return answer;
+    }
+}
