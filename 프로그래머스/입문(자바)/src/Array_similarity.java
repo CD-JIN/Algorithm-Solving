9d1b57772d777ddd2a2239071a61a05d316af9d2
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-01-09 23:55:19 +0900

    배열의 유사도

---
+++ 프로그래머스/입문(자바)/src/Array_similarity.java
@@ -0,0 +1,15 @@
+
+public class Array_similarity {
+	public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String str1 : s1) {
+            for(String str2 : s2 ) {
+            	if(str1.equals(str2)) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
